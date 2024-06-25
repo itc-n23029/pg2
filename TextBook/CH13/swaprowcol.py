100c7322a197a3ea0e7b9a2d60b36fa5a7a46083
itc-n23029 <n23029@std.it-college.ac.jp>
2024-06-25 03:30:48 +0000

    chapter13

---
+++ TextBook/CH13/swaprowcol.py
@@ -0,0 +1,22 @@
+import openpyxl
+import sys
+
+if len(sys.argv) < 2:
+    sys.exit('Usage: python swaprowcol.py example.xlsx')
+
+src = sys.argv[1]
+
+wb = openpyxl.load_workbook(src)
+sheet = wb.active
+
+new_wb = openpyxl.Workbook()
+new_sheet = new_wb.active
+
+for row in range(1, sheet.max_row + 1):
+    for col in range(1, sheet.max_column + 1):
+        old_cell = sheet.cell(column=col, row=row)
+        # 行rowと列colを入れ替える
+        new_cell = new_sheet.cell(column=row, row=col)
+        new_cell.value = old_cell.value
+
+new_wb.save(src + '.swap.xlsx')
